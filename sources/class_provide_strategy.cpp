60ad1744ccd89b3eced70c2205b46e562df8ebda
Disabled77 <dmitry.belous89@gmail.com>
2017-06-24 11:12:38 +0300

    implemented 3 ways of "create strategy"

--- sources/class_provide_strategy.cpp
+++ sources/class_provide_strategy.cpp
@@ -1,53 +1,95 @@
 #include <gtest/gtest.h>
+#include <utility>
 #include <iostream>
 
-enum class CreateStrategy{ NEW, MALLOC, PROTOTYPE };
-
-struct AnyStruct{
-    int value = 1;
-    AnyStruct(const AnyStruct& other):
-        value { other.value }
-    {}
-    AnyStruct():
-        value { 0 }
-    {}
-    AnyStruct(AnyStruct && other) = delete;
-};
-
-template <CreateStrategy>
-struct Create;
-
-template<>
-struct Create<CreateStrategy::NEW>{
-    template<class T, class... Args>
+template<class T>
+struct NewCreate{
+    template<class... Args>
     static T* create(Args... args){
         return new T { args... };
     }
 };
 
-template<>
-struct Create<CreateStrategy::MALLOC>{
-    template<class T, class... Args>
+template<class T>
+struct MallocCreate{
+    template<class... Args>
     static T* create(Args... args){
-        T* object = malloc(sizeof(T));
-        return new (object) T { args... };
+        T* object = reinterpret_cast<T*>(malloc(sizeof(T)));
+        new (object) T { args... };
+        return object;
     }
 };
 
-template<>
-struct Create<CreateStrategy::PROTOTYPE>{
-    Create()
+template<class T>
+struct PrototypCreate{
+    PrototypCreate(T* (functionPrototype)(T) = nullptr):
+        functionPrototype_ { functionPrototype }
+    {}
+
+    void setPrototype(T* (functionPrototype)(T)){
+        functionPrototype_ = functionPrototype;
+    }
+
+    T* getPrototype() const {
+        return functionPrototype_;
+    }
+
+    template<class... Args>
+    T* create(T value) const {
+        T* variable = functionPrototype_(value);
+        return variable;
+    }
+
+    T* (*functionPrototype_)(T);
+};
+
+template <class T>
+T* prototype(const T value){
+    T* result = reinterpret_cast<T*>(malloc(sizeof(T)));
+    new (result) T { value };
+    return result;
 }
+
+template <template <class T> class CreateStrategy>
+struct Creater{
+    using Type = int;
+    template <class... Args>
+    static Type* create(Args... args){
+        return CreateStrategy<Type>::create(args...);
+    }
+};
+
+template <>
+struct Creater<PrototypCreate>{
+    using Type = int;
+    using PrototypeCreateStrategy = PrototypCreate<Type>;
+    using Prototype = decltype(std::declval<PrototypCreate<Type>>().functionPrototype_);
+
+    Creater(Prototype functionPrototype):
+        prototypeCreator_(functionPrototype)
+    {}
+
+    template <class... Args>
+    Type* create(Args... args){
+        return prototypeCreator_.create(args...);
+    }
+    PrototypeCreateStrategy prototypeCreator_;
+};
 
 
 
 TEST(CLASS_PROVIDE_STRATEGY, choose_strategy)
 {
-   int *newInt = Create<CreateStrategy::NEW>::create<int>(1);
-   int *mallocInt = Create<CreateStrategy::NEW>::create<int>(2);
+   int *newInt = Creater<NewCreate>::create(1);
+   int *mallocInt = Creater<MallocCreate>::create(2);
+
+   Creater<PrototypCreate> creater(prototype);
+   int* byPrototype = creater.create(3);
+
 
    EXPECT_EQ(*newInt, 1);
    EXPECT_EQ(*mallocInt, 2);
+   EXPECT_EQ(*byPrototype, 3);
 
    delete newInt;
    delete mallocInt;
