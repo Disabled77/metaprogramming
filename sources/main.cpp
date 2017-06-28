5c9835674f65d3ec58f8e51a6b717fce11517145
Disabled77 <dmitry.belous89@gmail.com>
2017-06-28 08:38:45 +0300

    remove trash from main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
-template <std::size_t number>
-void print(){
-    std::cout<<"any number except zero: "<<number<<std::endl;
-}
-
-template <>
-void print<0ul>(){
-    std::cout<< "zero here"<<std::endl;
-}
-
-
-
 int main(int argc, char **argv){
 
     ::testing::InitGoogleTest(&argc, argv);
